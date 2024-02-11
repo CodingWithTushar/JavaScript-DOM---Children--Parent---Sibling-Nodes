c8ef08230119e8020a803bbc89bbd4dcf2b9355b
Tushar <tushard3998@gmail.com>
2024-02-11 23:03:38 +0530

    first commit

---
+++ scipt.js
@@ -0,0 +1,43 @@
+console.log("Hello World")
+
+document.body
+
+document.body.childNodes
+
+document.body.childNodes [1]
+
+document.body.childNodes[1].childNodes
+
+let cont = document.body.childNodes [1]
+
+cont
+
+cont.firstChild
+
+cont.lastChild
+
+cont.lastElementChild
+
+cont.lastElementChild.style.backgroundColor ="red"
+
+cont.lastElementChild.parentElement
+
+cont.firstElementChild.style.backgroundColor = "yellow"
+
+document.body.firstElementChild.children[3]
+
+document.body.firstElementChild.children[2]
+
+document.body.firstElementChild.children[2].parentElement
+
+document.body.firstElementChild.children [3].nextElementSibling
+
+document.body.firstElementChild.children [3].previousElementSibling
+
+document.body.children
+
+document.body.children[1]
+
+document.body.children[1].rows
+
+document.body.children[1].columns
